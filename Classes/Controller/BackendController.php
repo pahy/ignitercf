ccecacaaed60906c3bfc1f68783f8f493009a621
Patrick Hayder <patrick@hayder.org>
2026-01-18 10:28:21 +0100

    feat: Add module title to docheader

--- Classes/Controller/BackendController.php
+++ Classes/Controller/BackendController.php
@@ -38,6 +38,7 @@ final class BackendController extends ActionController
     public function indexAction(): ResponseInterface
     {
         $moduleTemplate = $this->moduleTemplateFactory->create($this->request);
+        $moduleTemplate->setTitle('IgniterCF - Cloudflare Cache Management');
         $this->addDocHeaderMenu($moduleTemplate, 'index');
 
         // Get user's days preferences
@@ -83,6 +84,7 @@ final class BackendController extends ActionController
     public function configurationAction(): ResponseInterface
     {
         $moduleTemplate = $this->moduleTemplateFactory->create($this->request);
+        $moduleTemplate->setTitle('IgniterCF - Cloudflare Cache Management');
         $this->addDocHeaderMenu($moduleTemplate, 'configuration');
 
         // Get all sites with their configuration status
